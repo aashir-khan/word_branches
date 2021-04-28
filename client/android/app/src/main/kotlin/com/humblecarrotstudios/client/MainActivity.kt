4d80b23fce4feae6b5b9e1a58f46d88ce7ea3fdc
Aashir Khan <aashir.khan@mail.utoronto.ca>
2021-04-27 22:46:30 -0400

    Add macos support

--- client/android/app/src/main/kotlin/com/humblecarrotstudios/client/MainActivity.kt
+++ client/android/app/src/main/kotlin/com/humblecarrotstudios/client/MainActivity.kt
@@ -1,6 +1,5 @@
-package com.humblecarrotstudios.client
+package com.humblecarrotstudios.wordbranches
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity() {}
